4747df5acf1235b0b79d62312d4cf8f6562dfda2
anOrgandroiD <anorgandroid@outlook.com>
2022-09-30 11:52:16 -0400

    created throwable apple + powerup switch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject bomb;
+    private int currentPower = 0;
+    public GameObject[] powerups;
     public float moveSpeed;
 
     // Start is called before the first frame update
@@ -16,10 +17,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (currentPower >= powerups.Length - 1)
+                currentPower = 0;
+            else
+                ++currentPower;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (currentPower <= 0)
+                currentPower = powerups.Length - 1;
+            else
+                --currentPower;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y + 0.5f);
-            SetBomb();
+            CreatePowerup();
         }
     }
 
@@ -29,8 +45,15 @@ public class PlayerController : MonoBehaviour
         transform.position = new Vector2(transform.position.x + (h * Time.deltaTime * moveSpeed), transform.position.y);
     }
 
-    void SetBomb()
+    void CreatePowerup()
     {
-        Instantiate(bomb, new Vector2(transform.position.x, transform.position.y - 0.5f), Quaternion.identity);
+        if (powerups[currentPower].CompareTag("Bomb"))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.5f);
+            Instantiate(powerups[currentPower], new Vector2(transform.position.x, transform.position.y - 0.5f), Quaternion.identity);
+        }
+        
+        else
+            Instantiate(powerups[currentPower], new Vector2(transform.position.x + 0.1f, transform.position.y), Quaternion.identity);
     }
 }
